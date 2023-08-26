60c918a66689d78a74b25073ce5539d2621a3f78
akshay77 <ajakshayjain11@gmail.com>
2023-08-26 20:19:06 +0530

    DSA

--- string facts/string.js
+++ string facts/string.js
@@ -1,9 +1,13 @@
 let a = "abc"
 let b = new String("abc")
 let c = String("abc")
-console.log(typeof c);
+// console.log(typeof c);
 
 // console.log(a === b);
 // console.log(typeof a);
 // console.log(typeof b);
-console.log(c===a);
+//console.log(c===a);
+
+let aj = "akhay"
+let kk = 20;
+console.log(aj.charAt(0));
