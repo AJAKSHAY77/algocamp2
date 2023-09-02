9b4ae6b7a4e68996d79e759373d676dcd5ec79f6
akshay77 <ajakshayjain11@gmail.com>
2023-09-02 18:06:36 +0530

    dsa

--- 12.2.js/imergesort.js
+++ 12.2.js/imergesort.js
@@ -1,4 +1,34 @@
-const arr = [1, 2, 3, 4]
-const arr2 = [22, 56, 44, 66]
 
-console.log([...arr,...arr2]);
+var merge = function (nums1, m, nums2, n) {
+  let i = 0;
+  let j = 0;
+  let k = 0;
+  let result = Array(m + n).fill(0);
+
+  while (i < m && j < n) {
+    if (nums1[i] < nums2[j]) {
+      result[k] = nums1[i];
+      i++;
+      k++;
+    } else {
+      result[k] = nums2[j];
+      j++;
+      k++;
+    }
+  }
+  while (i < m) {
+    result[k] = nums1[i];
+    i++;
+    k++;
+  }
+  while (j < n) {
+    result[k] = nums2[j];
+    j++;
+    k++;
+  }
+  for (i = 0; i < result.length; i++) {
+      nums1[i] = result[i];
+    }
+   console.log(nums1[i]);
+};
+merge()
--- 12.2.js/reverse.js
+++ 12.2.js/reverse.js
@@ -19,23 +19,39 @@
 // console.log(arr);
 
 
-let arr = [10, 20, 30, 40, 50, 90]
+// let arr = [10, 20, 30, 40, 50, 90]
 
-function swap(arr,i,j) {
-    let temp = arr[i];
+// function swap(arr,i,j) {
+//     let temp = arr[i];
+//     arr[i] = arr[j];
+//     arr[j] = temp;
+// }
+
+// let i = 0;
+// let j = arr.length - 1;
+// while (i < j) {
+//     swap(arr, i, j)
+//     i += 1;
+//     j-=1
+// }
+// console.log(arr);
+
+
+const arr = [1, 2, 3, 4, 5, 5, 6, 6]
+
+function swap(arr, i, j) {
+    const temp = arr[i];
     arr[i] = arr[j];
-    arr[j] = temp;
+    arr[j]=temp
 }
-
 let i = 0;
 let j = arr.length - 1;
 while (i < j) {
-    swap(arr, i, j)
-    i += 1;
-    j-=1
+    swap(arr, i, j);
+    i++;
+    j--;
 }
 console.log(arr);
-
 
 
 
