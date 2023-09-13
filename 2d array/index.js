7f7e18c1a36fffbc6025293e7d89e13d5fe9cc2e
akshay77 <ajakshayjain11@gmail.com>
2023-09-14 00:43:33 +0530

    sums

--- 2d array/index.js
+++ 2d array/index.js
@@ -20,10 +20,31 @@
 
 // make matrix of 7row and 5col
 
+// let arr = Array(7)
+// for (i = 0; i < 7; i++){
+//     let inner = Array(5).fill("a")
+//     arr[i] = inner
+    
+// }
+//console.log(arr);
+
+// let arr = Array(4)
+// for (i = 0; i < 4; i++){
+//     let inner = Array(4).fill("a")
+//  arr[i]=inner
+// }
+// console.log(arr);
+
+
+//7*5
 let arr = Array(7)
 for (i = 0; i < 7; i++){
-    let inner = Array(5).fill("a")
-    arr[i] = inner
-    
+    let inner = Array(5).fill("aj")
+    arr[i]=inner
 }
 console.log(arr);
+
+arr[0][1] = "akshay"
+arr[3][3] = "jain"
+console.log(arr);
+
