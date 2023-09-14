13fda9703ac25dbf7006b271fc9a8a73ccb6621d
akshay77 <ajakshayjain11@gmail.com>
2023-09-15 01:03:33 +0530

    addes

---
+++ subarray.js
@@ -0,0 +1,31 @@
+const arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10]
+
+// for (start = 0; start < arr.length; start++){
+//     let str = "";
+//     for (end = start; end < arr.length; end++){
+//         str = str + arr[end] + " "
+//         console.log(str);
+//     }
+// }
+
+
+// const arr2 = [1, 2, 3, 4]
+// for (start = 0; start < arr2.length; start++) {
+//     let str = "";
+//     for (end = start; end < arr2.length; end++){
+//         str = str + arr2[end] + " "
+        
+ 
+//         console.log(str);
+//     }
+// }
+
+const arr2 = [2, 2, 1, 1, 1, 2, 2]
+let result = 0
+for (i = 0; i < arr2.length; i++){
+result = result^arr2[i]    
+
+}
+console.log(result);
+
+  
