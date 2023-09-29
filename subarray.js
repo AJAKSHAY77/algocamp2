4a8e9830d727ed1aa1cfa1d6bd82699d63310d1c
akshay77 <ajakshayjain11@gmail.com>
2023-09-29 19:09:23 +0530

    added

--- subarray.js
+++ subarray.js
@@ -1,12 +1,3 @@
-const arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10]
-
-// for (start = 0; start < arr.length; start++){
-//     let str = "";
-//     for (end = start; end < arr.length; end++){
-//         str = str + arr[end] + " "
-//         console.log(str);
-//     }
-// }
 
 
 // const arr2 = [1, 2, 3, 4]
@@ -20,12 +11,39 @@ const arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10]
 //     }
 // }
 
-const arr2 = [2, 2, 1, 1, 1, 2, 2]
-let result = 0
-for (i = 0; i < arr2.length; i++){
-result = result^arr2[i]    
+// const arr2 = [2, 2, 1, 1, 1, 2, 2]
+// let result = 0
+// for (i = 0; i < arr2.length; i++){
+// result = result^arr2[i]
 
+// }
+// console.log(result);
+
+
+
+
+
+const subarray = [1,2,3,4,5,6,7,8]
+
+for (start = 0; start < subarray.length; start++){
+             let str = "";
+   
+    for (end = start; end < subarray.length; end++){
+       
+        str += subarray[end] + " ";
+        console.log(str);
+    }
 }
-console.log(result);
+
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 7, 8, 9, 10];
+
+// for (start = 0; start < arr.length; start++) {
+//   let str = "";
+//   for (end = start; end < arr.length; end++) {
+//     str = str + arr[end] + " ";
+//     console.log(str);
+//   }
+// }
 
   
